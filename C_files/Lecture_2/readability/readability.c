20442c022fca53de99c6d5e02a6396180b168fcd
shiwendukumar <shiwendugeorgian@gmail.com>
2024-01-05 12:43:12 +0530

    Completed Readability Problem

--- C_files/Lecture_2/readability/readability.c
+++ C_files/Lecture_2/readability/readability.c
@@ -4,21 +4,27 @@
 #include <stdio.h>
 #include <string.h>
 
+//Function Declaration
 int number_of_letters(string input);
 int number_of_words(string input);
 int number_of_sentences(string input);
 
 int main()
 {
+    //Get input from user
     string input = get_string("Text :");
+
+    //Calculate number of letter, word, sentences
     int letter = number_of_letters(input);
     int words = number_of_words(input);
     int sentences = number_of_sentences(input);
 
+    //Calculate Grade
     float L = ((float) letter / (float) words) * 100.0;
     float S = ((float) sentences / (float) words) * 100.0;
     int grade = round(0.0588 * L - 0.296 * S - 15.8);
 
+    //PRINT RESULTS
     if (grade >= 16)
     {
         printf("Grade 16+\n");
@@ -35,6 +41,7 @@ int main()
     return 0;
 }
 
+//Count number of letters
 int number_of_letters(string input)
 {
     int count = 0;
@@ -48,6 +55,7 @@ int number_of_letters(string input)
     return count;
 }
 
+//Count number of words
 int number_of_words(string input)
 {
     int count = 1;
@@ -61,6 +69,7 @@ int number_of_words(string input)
     return count;
 }
 
+//Count number of sentences
 int number_of_sentences(string input)
 {
     int count = 0;
