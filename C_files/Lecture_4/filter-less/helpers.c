57f75368a24872c8dc8f5396d049dae1d9d3b220
shiwendukumar <shiwendugeorgian@gmail.com>
2024-01-13 17:05:58 +0530

    Grayscale function completed

--- C_files/Lecture_4/filter-less/helpers.c
+++ C_files/Lecture_4/filter-less/helpers.c
@@ -1,8 +1,19 @@
 #include "helpers.h"
+#include <math.h>
 
 // Convert image to grayscale
 void grayscale(int height, int width, RGBTRIPLE image[height][width])
 {
+    for (int i = 0; i < height; i++)
+    {
+        for (int j = 0; j < width; j++)
+        {
+            int average = round((image[i][j].rgbtRed + image[i][j].rgbtGreen + image[i][j].rgbtBlue)/3.0);
+            image[i][j].rgbtRed = average;
+            image[i][j].rgbtGreen = average;
+            image[i][j].rgbtBlue = average;
+        }
+    }
     return;
 }
 
